a8f8552bfa15b2fd4719667f00f27eb6880faf69
darjafedosova <fedosova.d@gmail.com>
2023-08-29 14:07:02 +0300

    29.08

---
+++ src/Comand.java
@@ -0,0 +1,8 @@
+public class Comand {
+    public static void main(String[] args) { // Начинаем с этого :)
+
+        byte a1 = 1; // инициализация переменной
+        System.out.println("test = " + a1); // вывад на экран  в скобках название переменной + значение
+
+    }
+}
--- src/lesson2/MySecondClass.java
+++ src/lesson2/MySecondClass.java
@@ -1,4 +1,31 @@
 package lesson2;
 
 public class MySecondClass {
+    public static void main (String[] args){
+        byte a1 = 1;
+        short a2 = 30000;
+        int a3 = 1000000;
+        long a4 = 10000000;
+
+        System.out.println ("test = "  +a1);
+        System.out.println ("a2 = " + a2);
+        System.out.println ("a3 = " + a3);
+        System.out.println ("a4 = "+ a4);
+
+        float a5 = 123.456f;
+        double a6 = 1234456.12344;
+
+        System.out.println ("a5 = "  +a5);
+        System.out.println ("a6 = " + a6);
+
+
+        int x=1000;
+        int y=100;
+        int result;
+
+        result = x * y;
+
+        System.out.println ("Result = " + result);
+
+    }
 }
