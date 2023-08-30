8ecbececacb99c6d7a47bb5cf4c135f7b5d4bc1e
darjafedosova <fedosova.d@gmail.com>
2023-08-30 10:36:05 +0300

    29.08

--- src/lesson2/HomeWork2.java
+++ src/lesson2/HomeWork2.java
@@ -1,12 +1,12 @@
 package lesson2;
 
 import java.util.Scanner;
-
 public class HomeWork2 {
     public static void main(String[] args) {
 
-        System.out.print(" Task 1  \n\r");
-        /*System.out.println();*/
+
+        System.out.print( " Task 1  \n\r");
+
 
         int a1 = 89;
         byte a2 = 4;
@@ -16,18 +16,26 @@ public class HomeWork2 {
         long a6 = 12121;
         char a7 = 'G';
 
-        System.out.println ("a1 = " + a1 );
-        System.out.println ("a2 = " + a2);
-        System.out.println ("a3 = " + a3);
-        System.out.println ("a4 = " + a4);
-        System.out.println ("a5 = " + a5);
-        System.out.println ("a6 = " + a6);
-        System.out.println ("a7 = " + a7);
+
+        System.out.println("char: " + a7);
+        System.out.println("int: " + a1);
+        System.out.println("byte: " + a2);
+        System.out.println("short: " + a3);
+        System.out.println("float: " + a4);
+        System.out.println("double: " + a5);
+        System.out.println("long: " + a6);
 
 
         System.out.print(" Task 2  \n\r");
+        //Первый вариант решения
+        int a = 345;
+        int b = a / 100;
+        int c = b + 1;
+        int r = c + 1;
 
+        System.out.println("Number " + a + "-> " + b + ", " + c + ", " + r);
 
+        // Второй вариант решения
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter number: ");
@@ -37,10 +45,11 @@ public class HomeWork2 {
         scanner.close();
 
         int first = x / 100;
-        int second = ( x / 10) % 10;
+        int second = (x / 10) % 10;
         int third = x % 10;
 
-        System.out.println( " -> " + first + ", " + second + ", " + third);
+        System.out.println(" -> " + first + ", " + second + ", " + third);
 
     }
-    }
+}
+
