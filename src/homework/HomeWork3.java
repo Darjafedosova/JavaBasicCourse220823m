31d058963b4f0ea4a86b6f1b07e39d78d5d19c61
darjafedosova <fedosova.d@gmail.com>
2023-09-01 11:37:45 +0300

    Fedosova

---
+++ src/homework/HomeWork3.java
@@ -0,0 +1,44 @@
+package homework;
+public class HomeWork3 {
+    public static void main(String[] args){
+        String myString = new String("I study Basic Java!");
+        printString(myString);
+
+    }
+    public static void printString(String inputString) {
+        System.out.println(inputString);
+
+
+        char preLastChar = inputString.charAt(inputString.length() - 2);
+        System.out.println("Пред-последний символ: " + preLastChar);
+
+
+        boolean containsJava = inputString.contains("Java");
+        System.out.println("Строка содержит 'Java': " + containsJava);
+
+        String stringWithoutJava = inputString.substring(0, inputString.indexOf("Java"))
+                + inputString.substring(inputString.indexOf("Java") + 4);
+        System.out.println("Строка без 'Java': " + stringWithoutJava);
+
+        String replacedString = inputString.replace('а', 'о');
+        System.out.println("Строка после замены: " + replacedString);
+
+
+        String upperCaseString = inputString.toUpperCase();
+        System.out.println("Строка в верхнем регистре: " + upperCaseString);
+
+        String lowerCaseString = inputString.toLowerCase();
+        System.out.println("Строка в нижнем регистре: " + lowerCaseString);
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
